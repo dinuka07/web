cf01fac83e79ce37da94456f0062772050d797ee
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2023-07-17 06:50:58 +0100

    new

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
 <div class="nav-container">
