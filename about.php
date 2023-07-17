300a56b7dfec420dbc117959b443a3fccad22098
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2023-07-17 06:14:30 +0100

    new

--- about.php
+++ about.php
@@ -1,6 +1,17 @@
-<?php
-    include 'header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="about.css">
+</head>
+<body>
+
+  <?php
+    include 'nav.php';
+  ?>
+    
 
 <div class="card-hero">
 
@@ -278,9 +289,8 @@
 
 </div>
 
+</body>
+</html>
 
 
 
-<?php
-    include 'footer.php';
-?>
